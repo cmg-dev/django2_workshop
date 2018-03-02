28d63827bf12105fbba1eecbb92d0651278eb317
Christoph <christoph.gnip@gmail.com>
2018-03-02 17:57:04 +0100

    :bug: Fix Bug in Test

--- modules/solver/linear_equation.py
+++ modules/solver/linear_equation.py
@@ -13,18 +13,18 @@ def solve(A, b):
     We solve for x = A^-1 * b
 
     :A: The coefficient matrix for A.
-    :B: The result vector b.
+    :b: The result vector b.
     :returns: The solution to this equation system x as np.vector().
 
     EXAMPLES:
     ---------
     >>> solve([[1, 2], [3, 5]], [[10], [20]])
     matrix([[-10.],
-            [10.]])
+            [ 10.]])
 
     >>> solve([[1, 1], [2, 4]], [[35], [94]])
-    matrix([[ 23.],
-            [ 12.]])
+    matrix([[23.],
+            [12.]])
     """
     import numpy as np
     import numpy.linalg as mat
