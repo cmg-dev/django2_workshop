3aadbfd0a9e1019c2bb91226a1ecec057346425f
Christoph <christoph.gnip@gmail.com>
2018-03-02 01:28:07 +0100

    :sparkles: Add a New View, Update Hello World

--- newspaper/demo/urls.py
+++ newspaper/demo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:greeting>/', views.test_with_argument),
 ]
--- newspaper/demo/views.py
+++ newspaper/demo/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world.")
+
+def test_with_argument(request, greeting):
+    return HttpResponse('{}'.format(greeting))
